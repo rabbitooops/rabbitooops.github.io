defb4fcece1050044b1b6e8da5242abd42b4f9d8
rabbit <rabbitmeow886@gmail.com>
2020-06-18 17:07:54 +0800

    Add files via upload
    Change-Id: Iee15f26a64cd8ea8f45e12d229c7451f36a66913

---
+++ precache-manifest.9f6ac8ddeea8a62f0f3731bf4c0f8c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3676c375dea08236cbe30389441217f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dad868c2bbd6d1dc262",
+    "url": "/static/css/main.8a0a2dd2.chunk.css"
+  },
+  {
+    "revision": "bc938c6c0e8d9d568822",
+    "url": "/static/js/2.adbbcd11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.adbbcd11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dad868c2bbd6d1dc262",
+    "url": "/static/js/main.a09f39da.chunk.js"
+  },
+  {
+    "revision": "60e0e47ff47219119331",
+    "url": "/static/js/runtime-main.542bdb56.js"
+  }
+]);
